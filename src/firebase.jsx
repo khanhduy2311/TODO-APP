3e4b204dc9b7f66a1b8a510ef296c14cb76205c7
khanhduy2311 <khanhduy23112006@gmail.com>
2025-09-09 09:53:30 +0700

    deploy

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,14 +1,17 @@
+// src/firebase.jsx - PHIÊN BẢN ĐÚNG ĐỂ DEPLOY
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDogSmk0XH31D7qbzAj07D2P52ClcG_dFk",
-  authDomain: "naver-todo-app.firebaseapp.com",
-  projectId: "naver-todo-app",
-  storageBucket: "naver-todo-app.firebasestorage.app",
-  messagingSenderId: "42431800481",
-  appId: "1:42431800481:web:47e7486c447b1dc5377ce0",
-  measurementId: "G-CZ2HHY1DN1"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
