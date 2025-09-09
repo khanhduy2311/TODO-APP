39c9da6739ad1830482ca9ecbae5b7085bd01e9a
khanhduy2311 <khanhduy23112006@gmail.com>
2025-09-09 21:29:22 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -114,12 +114,12 @@ function App() {
         </button>
         <div className={`dropdown-menu ${isMenuOpen ? 'active' : ''}`}>
           <ul>
-            <li><button>Tasks</button></li>
             <li>
               <button onClick={toggleTheme}>
                 {theme === 'light' ? 'Dark Mode 🌙' : 'Light Mode ☀️'}
               </button>
             </li>
+            <li><button>Tasks</button></li>
             <li><button onClick={handleLogout}>Sign Out</button></li>
           </ul>
         </div>
